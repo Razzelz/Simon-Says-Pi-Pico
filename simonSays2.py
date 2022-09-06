3ed94cae6fcef2067b1a5b4719a7edfeba4ceb0f
D00397898 <72514630+D00397898@users.noreply.github.com>
2022-09-05 19:01:01 -0600

    Add files via upload

--- simonSays2.py
+++ simonSays2.py
@@ -1,9 +1,9 @@
-from machine import Pin, Timer
+from machine import Pin, Timer, PWM
 import random, time
-from machine import Pin, PWM
 
 randomIntList = []
 user_input = []
+buzzer = PWM(Pin(17))
 
 leds = [
 	Pin(11, Pin.OUT), #Red
@@ -22,41 +22,33 @@ buttons = [
 ]
 
 def waitForButton():
-	now = time.ticks_ms()
-	while time.ticks_ms() - now < 3000:
-		#time allowance has not expired
-		#listen for button presses
-		for i in range(len(buttons)):
-			if buttons[i].value() == 0: #This means the button is pushed down
-				leds[i].value(1)
-				while buttons[i].value() == 0:
-					pass
-				time.sleep(0.05)
-				leds[i].value(0)
-				return i
-	return -1
+    now = time.ticks_ms()
+    while time.ticks_ms() - now < 3000:
+        #time allowance has not expired
+        #listen for button presses
+        for i in range(len(buttons)):
+            if buttons[i].value() == 0: #This means the button is pushed down
+                leds[i].value(1)
+                while buttons[i].value() == 0:
+                    frequency = 800 // (i+1)
+                    buzzer.freq(frequency)
+                    buzzer.duty_u16(32768)
+
+                buzzer.duty_u16(30)
+                time.sleep(0.05)
+                leds[i].value(0)
+                return i
+    return -1
 
 def checkUserInput(): #Makes sure user input matches pattern
     for i in range(len(randomIntList)):
         user_input.append(waitForButton())
         if user_input[i] != randomIntList[i]:
-            gameOver()
             roundCount()
             randomIntList.clear() #Clear here because game is over
             user_input.clear() #Clear here because game is over
             return
     user_input.clear() #Clear here to prepare list for next comparison
-
-def gameOver():
-    for i in range(len(leds)):
-        leds[i].value(1)
-        time.sleep(0.08)
-        leds[i].value(0)
-    for i in range(len(leds)-1, -1, -1):
-        leds[i].value(1)
-        time.sleep(0.08)
-        leds[i].value(0)
-    print("GAME OVER")
 
 def roundCount(): #Flashes all lights to denote round number
     round_count = 0
@@ -68,13 +60,17 @@ def roundCount(): #Flashes all lights to denote round number
         leds[2].value(1)
         leds[3].value(1)
         leds[4].value(1)
+        buzzer.freq(550)
+        buzzer.duty_u16(32768)
         time.sleep(0.4)
+        buzzer.duty_u16(0)
         leds[0].value(0)
         leds[1].value(0)
         leds[2].value(0)
         leds[3].value(0)
         leds[4].value(0)
         time.sleep(0.4)
+    waitToStart()
 
 def randomLED(): #Generates random LED pattern
     randomLED = random.randint(0,4)
@@ -82,16 +78,20 @@ def randomLED(): #Generates random LED pattern
     for i in range(len(randomIntList)):
         time.sleep(0.5)
         leds[randomIntList[i]].value(1)
+
+        frequency = 800 // (randomIntList[i]+1)
+        buzzer.freq(frequency)
+        buzzer.duty_u16(32768)
         time.sleep(0.5)
         leds[randomIntList[i]].value(0)
+        buzzer.duty_u16(0)
 
-#This code doesn't work, and I can't fix it
-#def waitToStart(): #Waits for a button push before starting the game
-#    while True:
-#        for i in range(len(buttons)):
-#            if buttons[i].value == 0:
-#                return
-    
+def waitToStart(): #Waits for a button push before starting the game
+    while True:
+        for i in range(len(buttons)):
+            if buttons[i].value() == 0:
+                return
+
 def loop():
     randomLED()
     checkUserInput()
